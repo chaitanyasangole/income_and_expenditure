acc4d62feb002930fb0d80e1c3b80640bedc89c6
chaitanyasangole <48517745+chaitanyasangole@users.noreply.github.com>
2019-06-09 15:49:05 +0530

    File for login authentication created

---
+++ login.php
@@ -0,0 +1,21 @@
+<html>
+  <body>
+  <div class="row">
+    <div class="col-sm-4"></div>
+    <div class="col-sm-4">
+      <div class="card">
+        <div class="card-header">
+          Log In
+        </div>
+        <div class="card-body">
+          <label for="name"></label>
+          <div class="form-group">
+            <input type="text" id="name" name="username" maxlength="20" required class="form-control">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-4"></div>
+  </div>
+  </div>
+</html>
