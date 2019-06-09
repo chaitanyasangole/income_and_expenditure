bff03e6b67bfa6db75e5e884086c7052605ba0fd
chaitanyasangole <48517745+chaitanyasangole@users.noreply.github.com>
2019-06-09 15:52:36 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <html>
+  <head></head>
   <body>
   <div class="row">
     <div class="col-sm-4"></div>
@@ -8,14 +9,18 @@
           Log In
         </div>
         <div class="card-body">
-          <label for="name"></label>
+          <label for="name">Username</label>
           <div class="form-group">
             <input type="text" id="name" name="username" maxlength="20" required class="form-control">
+          </div>
+          <label for="pass">Passsword</label>
+          <div class="form-group">
+            <input type="password" id="pass" name="password" maxlength="20" required class="form-control">
           </div>
         </div>
       </div>
     </div>
     <div class="col-sm-4"></div>
   </div>
-  </div>
+  </body>
 </html>
